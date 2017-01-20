03d8a18167f5634620c50304ffb0c38516ea001d
kohku <kohku76@gmail.com>
2017-01-20 00:12:09 -0600

    Editable and most of the hotel widget

--- src/index.js
+++ src/index.js
@@ -2,10 +2,24 @@ import { WidgetZ } from './widgetz'
 
 export function main(params){
 
-//   console.log(JSON.stringify(params))
+  //  Check: compatibility
+  if (!Array.isArray(params)){
+    throw error('Invalid WidgetZVars')
+  }
+
+  let linkUrl = params.find(param => param.key === 'wdgt_link_url')
+
+  if (typeof linkUrl === 'undefined'){
+    params.push({ key: 'wdgt_link_url', value: global.wdgt_link_URL || null })
+  }
+
+  let pictureUrl = params.find(param => param.key === 'wdgt_imaage_url')
+
+  if (typeof pictureUrl === 'undefined'){
+    params.push({ key: 'wdgt_imaage_url', value: global.wdgt_image_URL || null })
+  }
 
   let widget = new WidgetZ(params)
-
 }
 
 main(widgetZVars || {});
--- src/stayingView.js
+++ src/stayingView.js
@@ -18,7 +18,55 @@ export class stayingView extends baseView{
   initialize(){
     const service = new WidgetService()
 
-    this.content.find('input[name="daterange"]').daterangepicker({
+    let pictureUrl = this.widget.params.find(p => p.key === 'wdgt_imaage_url')
+    if (pictureUrl){
+      $.each(this.content.find('.wdgtz_front .wdgtz_canvas'), (index, canvas) => {
+        let canva$ = $(canvas)
+        canva$.css('background-image', `url(${pictureUrl.value})`)
+        canva$.html(canva$.html().replace('Picture', ''))
+      })
+    }
+
+    // TODO: get location somewhere
+    let location = null
+    if (location){
+      $.each(this.content.find('.wdgtz_back .wdgtz_canvas'), (index, canvas) => {
+        let canva$ = $(canvas)
+        canva$.css('background-image', `url(${location})`)
+        canva$.html(canva$.html().replace('Map', ''))
+      })
+    }
+
+    // Set calendar range
+    let checkInDateOrDays = this.widget.params.find(p => p.key === 'check_in')
+    let checkIn = moment()
+    // is a number, calculate from now
+    if (Number.isInteger(checkInDateOrDays.value)){
+      checkIn.add(parseInt(checkInDateOrDays.value), 'days')
+    // is moment?
+    } else if (checkInDateOrDays.value instanceof moment){
+      checkIn = checkInDateOrDays.value
+    // is date string 'YYYY-MM-DD' ISO 8601
+    } else {
+      checkIn = moment(checkInDateOrDays.value)
+    }
+
+    let checkOutDateOrDays = this.widget.params.find(p => p.key === 'check_out')
+
+    let checkOut = moment()
+    // is a number, calculate from now
+    if (Number.isInteger(checkOutDateOrDays.value)){
+      checkOut.add(parseInt(checkOutDateOrDays.value), 'days')
+    // is moment?
+    } else if (checkOutDateOrDays.value instanceof moment){
+      checkOut = checkOutDateOrDays.value
+    // is date string 'YYYY-MM-DD' ISO 8601
+    } else {
+      checkOut = moment(checkOutDateOrDays.value)
+    }
+
+    let rangePicker = this.content.find('input[name="daterange"]')
+    rangePicker.daterangepicker({
         presetRanges: [],
         initialText: 'SELECT TRAVEL DATES',
         autoFitCalendars: true,
@@ -32,12 +80,6 @@ export class stayingView extends baseView{
             minDate: 0,
             maxDate: null,
             dayNamesMin: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
-            // onChangeMonthYear: function(year, month, instance){
-            //     debugger;
-            // },
-            // onSelect: function(dateText, instance){
-            //     debugger;
-            // }
         },
         open: (event, calendar) => {
             // put it at the bottomo of the control triggering the opening
@@ -46,7 +88,6 @@ export class stayingView extends baseView{
             this.content.find('.comiseo-daterangepicker.ui-widget').css('top', offset.top + el$.height())
             this.content.find('.comiseo-daterangepicker.ui-widget').css('left', offset.left)
 
-            // window.daterangepicker = calendar.instance
             var range = calendar.instance.getRange()
 
             if (range !== null){
@@ -68,24 +109,34 @@ export class stayingView extends baseView{
                         }
                     })
                 }
-                // TODO: Set event range background
             }
         },
     })
+    rangePicker.daterangepicker("setRange", { start: checkIn.toDate(), end: checkOut.toDate() });
 
     // load guest
-    $.each(this.content.find('.wdgtz_guest'), (index, guest) => {
-
-    })
+    let defaultGuests = this.widget.params.find(p => p.key === 'guests')
+    if (defaultGuests){
+      $.each(this.content.find('.wdgtz_guest'), (index, guests) => {
+        $(guests).val(defaultGuests.value || 1)
+      })
+    }
 
     // load rooms
-    $.each(this.content.find('.wdgtz_rooms'), (index, rooms) => {
-
-    })
+    let defaultRooms = this.widget.params.find(p => p.key === 'rooms')
+    if (defaultRooms){
+      $.each(this.content.find('.wdgtz_rooms'), (index, rooms) => {
+        $(rooms).val(defaultRooms.value || 1)
+      })
+    }
 
     // load hotel chain
-    $.each(this.content.find('.wdgtz_hotel-chain'), (index, chain) => {
-
+    service.getHotelChains().then(all => {
+      $.each(this.content.find('.wdgtz_hotel-chain'), (index, element) => {
+        all.forEach(chain => {
+          $(element).append(`<option value="${chain.key}">${chain.value}</option>`)
+        })
+      })
     })
 
     // load hotel raiting
@@ -104,11 +155,28 @@ export class stayingView extends baseView{
   }
 
   setListeners(){
-    // Flip for map picture
-    $.each(this.content.find('.wdgtz_flip'), (index, btn) => {
-      $(btn).on('click', () => {
-        $('.wdgtz_flip-container').toggleClass('wdgtz_hover');
+    // TODO: Setup listeners for edit
+    this.content.find('.wdgtz_edit').on('click', event => {
+      let address = $('.wdgtz_address1, .wdgtz_city, .wdgtz_state-province, .wdgtz_zip, .wdgtz_country, .wdgtz_full-address')
+      $.each(address, (index, element) => {
+        $(element).toggleClass('editable')
       })
+    })
+    
+
+    // TODO: We'll move this to mustache to save some code
+    let widgetUrl = this.widget.params.find(p => p.key === 'wdgt_link_url')
+    if (widgetUrl){
+      this.content.find('.wdgtz_front .wdgtz_canvas').on('click', event => {
+        window.open(widgetUrl.value, '_blank');
+      })
+    }
+
+    // Flip for map picture
+    this.content.find('.wdgtz_flip').on('click', event => {
+        event.preventDefault()
+        event.stopPropagation()
+        $('.wdgtz_flip-container').toggleClass('wdgtz_hover');
     })
 
     // More/fewer options listeners
@@ -116,6 +184,12 @@ export class stayingView extends baseView{
       event.preventDefault()
       $(event.currentTarget.parentElement).toggleClass('wdgtz_expanded')
       this.content.find('.wdgtz_action').toggleClass('wdgtz_expanded')
+    })
+
+    this.content.find('.wdgtz_action button').on('click', event => {
+      // TODO: do search 
+      console.log('Searching')
+      // window.open(url, '_blank')
     })
   }
 }
