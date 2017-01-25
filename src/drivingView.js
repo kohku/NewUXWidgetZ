ac240dfa538cbb78465ffc8fe2efa23778f77b44
kohku <kohku76@gmail.com>
2017-01-24 23:23:10 -0600

    Making progress with driving

--- src/drivingView.js
+++ src/drivingView.js
@@ -1,7 +1,32 @@
 import { baseView } from './baseView'
+import $ from 'jquery'
+// import timepicker from 'timepicker'
+// import 'timepicker/jquery.timepicker.css'
 
-export class drivingView extends baseView{
+export class drivingView extends baseView {
   constructor(widget, state, selector, content){
     super(widget, state, selector, content)
+  }
+
+  initialize(){
+  }
+
+  setListeners(){
+    // More/fewer options listeners
+    this.content.find('.wdgtz_options > label').on('click', event => this.toggleRentalCarOptions(event))
+    this.content.find('.wdgtz_options .wdgtz_params input[type="radio"]').on('change', event => this.toggleTripRule(event))
+  }
+
+  toggleTripRule(event){
+    if (event.currentTarget.value === 'oneway'){
+      this.content.find('.wdgtz_options .wdgtz_params').addClass('wdgtz_trip-rule-oneway')
+    } else {
+      this.content.find('.wdgtz_options .wdgtz_params').removeClass('wdgtz_trip-rule-oneway')
+    }
+  }
+
+  toggleRentalCarOptions(event){
+    event.preventDefault()
+    $(event.currentTarget.parentElement).toggleClass('wdgtz_expanded')
   }
 }
--- src/flyingView.js
+++ src/flyingView.js
@@ -1,6 +1,6 @@
 import { baseView } from './baseView'
 
-export class flyingView extends baseView{
+export class flyingView extends baseView {
   constructor(widget, state, selector, content){
     super(widget, state, selector, content)
   }
